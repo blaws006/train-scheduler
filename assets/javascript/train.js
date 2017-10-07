0d30a584e54c3f427dec11e9414361d5ae7668e9
blaws006 <blaws006>
2017-10-06 20:58:46 -0400

    Still trying to fix issue with format

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,22 +1,22 @@
 var config = {
-    apiKey: "AIzaSyBqonqE3MGfoorYnrICmQGDXIbwWooS9oY",
-    authDomain: "trainscheduler-de9a7.firebaseapp.com",
-    databaseURL: "https://trainscheduler-de9a7.firebaseio.com",
-    projectId: "trainscheduler-de9a7",
-    storageBucket: "trainscheduler-de9a7.appspot.com",
-    messagingSenderId: "252697521678"
-  };
-  firebase.initializeApp(config);
+	apiKey: "AIzaSyBqonqE3MGfoorYnrICmQGDXIbwWooS9oY",
+	authDomain: "trainscheduler-de9a7.firebaseapp.com",
+	databaseURL: "https://trainscheduler-de9a7.firebaseio.com",
+	projectId: "trainscheduler-de9a7",
+	storageBucket: "trainscheduler-de9a7.appspot.com",
+	messagingSenderId: "252697521678"
+};
+firebase.initializeApp(config);
 
- var database = firebase.database();
- var trainNum = 1;
+var database = firebase.database();
+var trainNum = 1;
 
- $("#submit-button").on("click", function(){
- 	event.preventDefault();
- 	var newTrainName = $("#name").val().trim();
- 	var newTrainDest = $("#destination").val().trim();
- 	var newTrainTime = $("#time").val().trim();
-	var newTrainFreq = $("#frequency").val().trim(); 
+$("#submit-button").on("click", function () {
+	event.preventDefault();
+	var newTrainName = $("#name").val().trim();
+	var newTrainDest = $("#destination").val().trim();
+	var newTrainTime = $("#time").val().trim();
+	var newTrainFreq = $("#frequency").val().trim();
 	console.log(newTrainName);
 	console.log(newTrainDest);
 	console.log(newTrainTime);
@@ -44,31 +44,31 @@ var config = {
 
 });
 
- database.ref().on("child_added", function(snapshot){
+database.ref().on("child_added", function (snapshot) {
 
- 	console.log(snapshot.val());
+	console.log(snapshot.val());
 
-	
- 	var newTrainName = snapshot.val().name;
- 	var newTrainDest = snapshot.val().dest;
- 	var newTrainTime = snapshot.val().time;
- 	var newTrainFreq = snapshot.val().freq;
 
- 	console.log(newTrainName);
- 	console.log(newTrainDest);
- 	console.log(newTrainTime);
- 	console.log(newTrainFreq);
+	var newTrainName = snapshot.val().name;
+	var newTrainDest = snapshot.val().dest;
+	var newTrainTime = snapshot.val().time;
+	var newTrainFreq = snapshot.val().freq;
+
+	console.log(newTrainName);
+	console.log(newTrainDest);
+	console.log(newTrainTime);
+	console.log(newTrainFreq);
 
 
 	var currentTime = moment().format("hh:mm:ss a")
- 	var nextTrain = moment(newTrainTime).add(newTrainFreq, "m");
- 	console.log(nextTrain._d);
+	var nextTrain = moment(newTrainTime).add(newTrainFreq, "m");
+	console.log(nextTrain._d);
 
- 	var newTrainFormat = moment(nextTrain).format("hh:mm:ss a")
- 	console.log(newTrainFormat);
- 	var minAway = moment.utc(moment(newTrainFormat, "HH:mm:ss").diff(moment(currentTime, "HH:mm:ss"))).format("mm");
- 	console.log(minAway)
+	var newTrainFormat = moment(nextTrain).format("h:mm a")
+	console.log(newTrainFormat);
+	var minAway = moment.utc(moment(newTrainFormat, "hh:mm").diff(moment(currentTime, "hh:mm"))).format("mm");
+	console.log(minAway)
 
- 	$("#schedule-info").append("<tr><th scope='row' id='train-num'>" + trainNum++ + "</th><td id='train-name'>" + newTrainName + "</td><td id='train-destination'>" + newTrainDest + "</td><td id='train-frequency'>" + newTrainFreq + "</td><td id='train-arrival'>" + newTrainFormat + "</td><td id='train-min-away'>" + minAway + "</td></tr>")
+	$("#schedule-info").append("<tr><th scope='row' id='train-num'>" + trainNum++ + "</th><td id='train-name'>" + newTrainName + "</td><td id='train-destination'>" + newTrainDest + "</td><td id='train-frequency'>" + newTrainFreq + "</td><td id='train-arrival'>" + newTrainFormat + "</td><td id='train-min-away'>" + minAway + "</td></tr>")
 
- });
+});
